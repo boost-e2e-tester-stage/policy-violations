bf3f235d912094fe999328d26f92ed9dfd747a1e
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-10-10 02:42:58 +0000

    Commit a vulnerability

--- goat.tf
+++ goat.tf
@@ -1,2 +1,25 @@
 
-# Goat.tf version 0.5474633909164244
+# Goat.tf version 0.5033572104999484
+resource "google_storage_bucket" "terragoat_website" {
+name          = "terragot-${var.environment}"
+force_destroy = true
+labels = {
+git_commit           = "ff3ee8387837a499665630ebb0371be39ce35fb6"
+git_file             = "terraform/gcp/gcs.tf"
+git_last_modified_at = "2020-07-08 12:02:14"
+git_last_modified_by = "nimrodkor@gmail.com"
+git_modifiers        = "nimrodkor"
+git_org              = "bridgecrewio"
+git_repo             = "terragoat"
+yor_trace            = "bd00cd2e-f53f-4daf-8d4d-74c47846c1cc"
+}
+logging {
+log_bucket = "secret.example.com-access-logs"
+log_object_prefix =  "log/secrets-"
+}
+}
+resource "google_storage_bucket_iam_binding" "allow_public_read" {
+bucket  = google_storage_bucket.terragoat_website.id
+members = ["allUsers"]
+role    = "roles/storage.objectViewer"
+}
